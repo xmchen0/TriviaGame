4519ccf7dcd71c6604117faaeb5dbfc160305cd3
Kathy Chen <kathychen.social@gmail.com>
2019-02-14 16:37:21 -0500

    uploading files

---
+++ assets/javascript/app.js
@@ -0,0 +1,25 @@
+/// ********** GLOBAL VARIABLE **********
+
+//1. timer
+
+//2. questions and answers
+//2.1 questions string
+//2.2 answer array
+//2.3 correct answer
+//2.4 incorrect answer 
+//2.5 unanswered
+
+
+/// ********** FUNCTIONS **********
+
+//1. start screen, click start button
+
+//2. starts game, counter starts countdown set at 60 seconds
+
+//3. user selects a multiple choice answer, only 1 answer per question
+
+//4. when time remaining hits zero, 
+//4.1 a separate screen will show all done 
+//4.2 display scoreboard - correct, incorrect answers and unanswered questions
+
+//5. alternatively user can press done if they finish early
